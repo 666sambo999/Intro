fe470053d8ca41a13238b6ce16f4171181d17715
Koros_zl65@top-academy.ru <Koros_zl65@top-academy.ru>
2023-08-17 00:43:10 +0500

    Parametrs

--- Point/Program.cs
+++ Point/Program.cs
@@ -11,6 +11,12 @@ namespace Point
         struct Point
         {
             public double x, y;
+            public static int count;
+
+            static Point ()
+            {
+                count = -1;
+            }
             public Point (double x , double y)
             {
                 this.x = x; 
@@ -20,17 +26,42 @@ namespace Point
             {
                 Console.WriteLine($" X= {x} \t Y = {y}" );
             }
-        }
+            public double distance(Point other)
+            {
+                //double x_distance = this.x - other.x;
+                //double y_distance = this.y - other.y;
+                //double distance = Math.Sqrt(x_distance* x_distance + y_distance* y_distance);       
+                //return distance;
+                return Math.Sqrt(Math.Pow(this.x-other.x,2)+Math.Pow(this.y-other.y,2));        
+                //класс Math - содержит набор статичестких методов, реаизующие математич функции 
+            }
+            public static double distance (Point left, Point right) 
+            { 
+                return left.distance(right);
+            }
 
+
+        }
+        
         // Heap 
         static void Main(string[] args)
         {
+            Console.WriteLine($" Количество точек: {Point.count}");
             Point A;
             A.x = 2;
             A.y = 3;
             A.print();
             Point B = new Point(8, 9);
             B.print();
+
+            Console.WriteLine($"Расстояние от точки А до точки В: {A.distance(B)}");
+            Console.WriteLine($"Расстояние от точки B до точки A: {B.distance(A)}");
+            Console.WriteLine($" Расстояние между точками *А* и *Б* {Point.distance(A,B)}");
+            Console.WriteLine($" Расстояние между точками *Б* и *А* {Point.distance(B, A)}");
+
+            Console.WriteLine($" Расстояние между точками *A* и *B*: {Point.distance(new Point(22, 33), new Point(77, 88))}"); ;
+            Console.WriteLine($" Расстояние между точками *b* и *a*: {new Point(88, 66).distance(new Point(22, 33))}"); 
+
         }
     }
 }
---
+++ TransferParametr/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TransferParametr
+{
+    internal class Program
+    {
+       static void Input (out int a,out int b)
+        {
+            Console.WriteLine($" Введите переменную а: ");
+            a= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($" Введите переменную в: ");
+            b = Convert.ToInt32(Console.ReadLine());
+        }
+        static void Exchange(ref int a, ref int b)
+        {
+            int buffer = a;
+            a = b;
+            b = buffer;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int a, b;
+            Input (out a, out b);
+            Console.WriteLine($" {a} \t {b}");
+            Exchange(ref a, ref b);
+            Console.WriteLine($" {a} \t {b}");
+
+
+
+        }
+    }
+}
